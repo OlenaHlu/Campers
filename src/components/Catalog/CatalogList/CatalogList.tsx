657e6e1a6e5bbf0bf51fa2713020460f63257e3d
OlenaHlu <glushenkova_es@ukr.net>
2025-05-30 21:18:43 +0300

    clear

--- src/components/Catalog/CatalogList/CatalogList.tsx
+++ src/components/Catalog/CatalogList/CatalogList.tsx
@@ -1,12 +1,8 @@
 import css from "./CatalogList.module.css";
-import CatalogItem from "../CatalogItem/CatalogItem";
+// import CatalogItem from "../CatalogItem/CatalogItem";
 
 const CatalogList = () => {
-  return (
-    <div className={css.listContainer}>
-      <CatalogItem />
-    </div>
-  );
+  return <div className={css.listContainer}>{/* <CatalogItem /> */}</div>;
 };
 
 export default CatalogList;
