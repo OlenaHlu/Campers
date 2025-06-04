1fe0423a637f8b66f44c563e716f00b2114f6bc7
OlenaHlu <glushenkova_es@ukr.net>
2025-06-05 02:02:50 +0300

    added erB-ry

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,30 @@
+import React, { Component, ErrorInfo } from "react";
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+};
+
+class ErrorBoundary extends Component<
+  { children: React.ReactNode },
+  ErrorBoundaryState
+> {
+  state: ErrorBoundaryState = { hasError: false };
+
+  static getDerivedStateFromError(_error: Error): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Щось пішло не так.Спробуйте ще раз.</h2>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
