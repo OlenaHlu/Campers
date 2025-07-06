241f8eae2519b0fb6b92f7dfb789073e354caf50
OlenaHlu <glushenkova_es@ukr.net>
2025-07-06 22:47:27 +0300

    added not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,25 @@
+import css from "./NotFoundPage.module.css";
+import { useNavigate } from "react-router-dom";
+
 const NotFoundPage = () => {
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate("/");
+  };
   return (
-    <div>
-      <p>404 - Page Not Found</p>
+    <div className={css.notFoundContainer}>
+      <div className={`${css.lostItem} ${css.item1}`}></div>
+      <div className={`${css.lostItem} ${css.item2}`}></div>
+      <div className={`${css.lostItem} ${css.item3}`}></div>
+      <h1 className={css.notFoundHeading}>404</h1>
+      <p className={css.notFoundMessage}>Oops! It seems you're lost.</p>
+      <p className={css.notFoundSubMessage}>This page does not exist.</p>
+      <button className={css.notFoundButton} onClick={handleGoHome}>
+        Go Home
+      </button>
+      <div className={`${css.lostItem} ${css.item4}`}></div>
+      <div className={`${css.lostItem} ${css.item5}`}></div>
     </div>
   );
 };
